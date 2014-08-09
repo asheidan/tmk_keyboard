2202232a67da4caa6a6e246e6adb4724eb81d996
MIW <email@example.com>
2014-08-09 22:15:03 +0200

    first commit.

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -32,6 +32,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap.h"
 #include "ergodox.h"
 
+// Swedish defines for easier reading of layout
+#define KC_AA   KC_LBRACKET
+#define KC_AE   KC_QUOTE
+#define KC_OE   KC_SCOLON
+#define KC_PLS  KC_MINUS           /* +, ? and \ */
+#define KC_QUOT KC_BSLASH          /* ' and * */
+#define KC_MINS KC_SLASH           /* - and _ */
+#define KC_LTE  KC_NONUS_BSLASH    /* <, > and | */
+#define KC_GRV  KC_EQUAL           /* ' (accent) and ` */
+#define KC_TLDE KC_RBRACKET        /* ", ^ and ~ */
+
+// Alias for functions so keymap get's easier to read
+/* layers */
+#define KC_L0   KC_FN20
+#define KC_L1   KC_FN21
+#define KC_L2   KC_FN22
+#define KC_L3   KC_FN23
+/* switched numbers and most common symbol without shift mod */
+#define KC_INV1 KC_FN1
+#define KC_INV2 KC_FN2
+#define KC_INV3 KC_FN3
+#define KC_INV4 KC_FN4
+#define KC_INV5 KC_FN5
+#define KC_INV6 KC_FN6
+#define KC_INV7 KC_FN7
+#define KC_INV8 KC_FN8
+#define KC_INV9 KC_FN9
+#define KC_INV0 KC_FN0
+/* unique special char keys */
+#define KC_LBRK KC_FN11           /* { and [  Left brackets */
+#define KC_RBRK KC_FN12           /* } and ]  Right brackets */
+#define KC_BSHA KC_FN13           /* Backslash and @ */
+#define KC_RTDE KC_FN14           /* Modified tilde. ~/ sequence, ^ and " */
+#define KC_IMNS KC_FN15           /* _ and -  Inverted minus */
+#define KC_SCLN KC_FN16           /* Only semicolon */
+#define KC_CLN  KC_FN17           /* Colon and pipe */
+#define KC_MNSA KC_FN18           /* -, ' (accent) and ` (grav accent) */
 
 /* ErgoDox keymap definition macro */
 #define KEYMAP(                                                 \
@@ -75,52 +112,79 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { KC_##k0D,KC_##k1D,KC_##k2D,KC_##k3D,KC_##k4D,KC_NO   }    \
    }
 
-#if defined(KEYMAP_DVORAK)
-#include "keymap_dvorak.h"
-#elif defined(KEYMAP_COLEMAK)
-#include "keymap_colemak.h"
-#elif defined(KEYMAP_WORKMAN)
-#include "keymap_workman.h"
-#elif defined(KEYMAP_MICRO)
-#include "keymap_micro.h"
-#elif defined(KEYMAP_CUB)
-#include "keymap_cub.h"
-#else
-
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(  // layer 0 : default
         // left hand
-        EQL, 1,   2,   3,   4,   5,   ESC,
-        BSLS,Q,   W,   E,   R,   T,   FN2,
+		L2,  INV1,INV2,INV3,INV4,INV5,INV6,
+        LTE, Q,   W,   E,   R,   T,   LBRK,
         TAB, A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   FN1,
-        LGUI,GRV, BSLS,LEFT,RGHT,
-                                      LCTL,LALT,
-                                           HOME,
-                                 BSPC,DEL, END,
+        ESC, Z,   X,   C,   V,   B,   HOME,
+        L1,  LEFT,UP,  DOWN,RGHT,
+                                      DEL, LALT,
+                                           F5,
+                                 LSFT,BSPC,RCTL,
         // right hand
-             FN3, 6,   7,   8,   9,   0,   MINS,
-             LBRC,Y,   U,   I,   O,   P,   RBRC,
-                  H,   J,   K,   L,   SCLN,QUOT,
-             FN1, N,   M,   COMM,DOT, SLSH,RSFT,
-                       LEFT,DOWN,UP,  RGHT,RGUI,
-        RALT,RCTL,
+             L3,  INV7,INV8,INV9,INV0,PLS, BSHA,
+             RBRK,Y,   U,   I,   O,   P,   AA,
+                  H,   J,   K,   L,   OE,  AE,
+             END, N,   M,   COMM,DOT, IMNS,CLN,
+			           QUOT,INS, RTDE,MNSA,SCLN,
+        RALT,RGUI,
         PGUP,
         PGDN,ENT, SPC
     ),
-
-    KEYMAP(  // layer 1 : function and symbol keys
+    KEYMAP(  // layer 1 : regular numbers
         // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  F11,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN4,
+        TRNS,1,   2,   3,   4,   5,   6,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        L0,  TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,LCTL,
+        // right hand
+             TRNS,7,   8,   9,   0,   TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                       TRNS,TRNS,TLDE,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
+    KEYMAP(  // layer 2 : f-keys, numpad, media keys, misc
+        // left hand
+        L0,  F1,  F2,  F3,  F4,  F5,  F6,
+        TRNS,VOLD,VOLU,MPLY,MPRV,TRNS,TRNS,
+        TRNS,TRNS,MUTE,MSTP,MNXT,TRNS,
+        TRNS,TRNS,PSCR,PAUS,SLCK,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             F12, F6,  F7,  F8,  F9,  F10, TRNS,
+             TRNS,F7,  F8,  F9,  F10, F11, F12,
+             TRNS,PMNS,P7,  P8,  P9,  NLCK,TRNS,
+                  PPLS,P4,  P5,  P6,  TRNS,TRNS,
+             TRNS,PAST,P1,  P2,  P3,  PSLS,TRNS,
+                       P0,  PDOT,NO,  TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
+    KEYMAP(  // layer 3: gaming layer (some games can't bind special char keys
+        // left hand
+        TAB, 1,   2,   3,   4,   5,   6,
+        H,   TRNS,TRNS,TRNS,TRNS,TRNS,END,
+        J,   TRNS,TRNS,TRNS,TRNS,TRNS,
+        K,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        L,   TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,TRNS,
+        // right hand
+             L0,  7,   8,   9,   0,   TRNS,ESC,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
                   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -129,79 +193,356 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,
         TRNS,TRNS,TRNS
     ),
-
-    KEYMAP(  // layer 2 : keyboard functions
-        // left hand
-        FN0, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             FN4, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // layer 3: numpad
-        // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             SLCK,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
-                  NO,  P4,  P5,  P6,  PPLS,PENT,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
 };
 
 /* id for user defined functions */
 enum function_id {
-    TEENSY_KEY,
+	INV_0,
+	INV_1,
+	INV_2,
+	INV_3,
+	INV_4,
+	INV_5,
+	INV_6,
+	INV_7,
+	INV_8,
+	INV_9,
+	FN_LCBRACKET,
+	FN_RCBRACKET,
+	FN_TILDE,
+	FN_UNDERSCR,
+	FN_MINUS_ACCENT,
+	FN_COLON_PIPE,
+	FN_BASHSLAH_AT,
+	SET_L1_WITH_LED,
+	SET_L2_WITH_LED,
+	SET_L3_WITH_LED,
+	SET_L0_LED_OFF,
 };
 
 /*
  * Fn action definition
  */
 static const uint16_t PROGMEM fn_actions[] = {
-    ACTION_FUNCTION(TEENSY_KEY),                    // FN0 - Teensy key
-    ACTION_LAYER_MOMENTARY(1),                      // FN1 - switch to Layer1
-    ACTION_LAYER_SET(2, ON_PRESS),                  // FN2 - push Layer2
-    ACTION_LAYER_SET(3, ON_PRESS),                  // FN3 - push Layer3
-    ACTION_LAYER_SET(0, ON_PRESS),                  // FN4 - push Layer0
+	[0] = ACTION_FUNCTION(INV_0),
+	[1] = ACTION_FUNCTION(INV_1),
+	[2] = ACTION_FUNCTION(INV_2),
+	[3] = ACTION_FUNCTION(INV_3),
+	[4] = ACTION_FUNCTION(INV_4),
+	[5] = ACTION_FUNCTION(INV_5),
+	[6] = ACTION_FUNCTION(INV_6),
+	[7] = ACTION_FUNCTION(INV_7),
+	[8] = ACTION_FUNCTION(INV_8),
+	[9] = ACTION_FUNCTION(INV_9),
+	[11] = ACTION_FUNCTION(FN_LCBRACKET),
+	[12] = ACTION_FUNCTION(FN_RCBRACKET),
+	[13] = ACTION_FUNCTION(FN_BASHSLAH_AT),
+	[14] = ACTION_FUNCTION(FN_TILDE),
+	[15] = ACTION_FUNCTION(FN_UNDERSCR),
+	[16] = ACTION_MODS_KEY(MOD_RSFT, KC_COMM),  // ;
+	[17] = ACTION_FUNCTION(FN_COLON_PIPE),
+	[18] = ACTION_FUNCTION(FN_MINUS_ACCENT),
+    [20] = ACTION_FUNCTION(SET_L0_LED_OFF),
+    [21] = ACTION_FUNCTION(SET_L1_WITH_LED),
+    [22] = ACTION_FUNCTION(SET_L2_WITH_LED),
+    [23] = ACTION_FUNCTION(SET_L3_WITH_LED),
 };
 
-void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
-{
-    if (id == TEENSY_KEY) {
-        clear_keyboard();
-        print("\n\nJump to bootloader... ");
-        _delay_ms(250);
-        bootloader_jump(); // should not return
-        print("not supported.\n");
-    }
+/* Set layer 3 and light up led on teensy board.
+ * Gaming layer
+ */
+void set_layer3_with_led(void) {
+	ergodox_right_led_3_off();
+	ergodox_right_led_2_off();
+	ergodox_right_led_1_on();
+	layer_clear();
+	layer_on(3);
 }
 
-#endif
+/* Set layer 2 and light up led on teensy board.
+ * Function layer
+ */
+void set_layer2_with_led(void) {
+	ergodox_right_led_1_off();
+	ergodox_right_led_2_off();
+	ergodox_right_led_3_on();
+	layer_clear();
+	layer_on(2);
+}
+
+/* Set layer 1 and light up led on teensy board.
+ * Number layer
+ */
+void set_layer1_with_led(void) {
+	ergodox_right_led_3_off();
+	ergodox_right_led_1_off();
+	ergodox_right_led_2_on();
+	layer_clear();
+	layer_on(1);
+}
+
+/* Set layer 0 and light up led on teensy board.
+ */
+void set_layer0_switch_led_off(void) {
+	layer_clear();
+	ergodox_led_all_off();
+}
+
+/* add a shifted mod if no mod,
+ * remove shited mod if shift mod only,
+ * let all other mod combinations through without alteration.
+ */
+void inv_key(uint8_t key) {
+	// invert shifting
+	if (get_mods() == MOD_BIT(KC_LSHIFT)) {
+		del_mods(MOD_BIT(KC_LSHIFT));
+	} else if (get_mods() == 0) {  // don't use else, this lets other mod combos through
+		add_mods(MOD_BIT(KC_LSHIFT));
+	}
+	register_code(key);
+	// reset mods, otherwise quick keypresses can result in bugs (wrong mod state)
+	if (get_mods() == MOD_BIT(KC_LSHIFT)) {
+		del_mods(MOD_BIT(KC_LSHIFT));
+	} else if (get_mods() == 0) {  // don't use else, this lets other mod combos through
+		add_mods(MOD_BIT(KC_LSHIFT));
+	}
+}
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    keyevent_t event = record->event;
+    tap_t tap = record->tap;
+
+	switch (id) {
+		case INV_0:
+			if (event.pressed) {
+				inv_key(KC_0);
+			} else {
+				unregister_code(KC_0);
+			}
+			break;
+		case INV_1:
+			if (event.pressed) {
+				inv_key(KC_1);
+			} else {
+				unregister_code(KC_1);
+			}
+			break;
+		case INV_2:
+            if (event.pressed) {
+				inv_key(KC_2);
+			} else {
+				unregister_code(KC_2);
+			}
+			break;
+		case INV_3:
+            if (event.pressed) {
+				inv_key(KC_3);
+			} else {
+				unregister_code(KC_3);
+			}
+			break;
+		case INV_4:
+            if (event.pressed) {
+				uint8_t mods = get_mods();
+				if (mods == 0) {  // $
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_4);
+					clear_mods();
+				} else if (mods == MOD_BIT(KC_LSHIFT)) {  // 4
+					clear_mods();
+					register_code(KC_4);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else if (mods == MOD_BIT(KC_RALT)) {  // ¤
+					set_mods(MOD_BIT(KC_LSHIFT));
+					register_code(KC_4);
+					set_mods(MOD_BIT(KC_RALT));
+				}
+			} else {
+				unregister_code(KC_4);
+			}
+			break;
+		case INV_5:
+            if (event.pressed) {
+				inv_key(KC_5);
+			} else {
+				unregister_code(KC_5);
+			}
+			break;
+		case INV_6:
+            if (event.pressed) {
+				inv_key(KC_6);
+			} else {
+				unregister_code(KC_6);
+			}
+			break;
+		case INV_7:
+            if (event.pressed) {
+				inv_key(KC_7);
+			} else {
+				unregister_code(KC_7);
+			}
+			break;
+		case INV_8:
+            if (event.pressed) {
+				inv_key(KC_8);
+			} else {
+				unregister_code(KC_8);
+			}
+			break;
+		case INV_9:
+            if (event.pressed) {
+				inv_key(KC_9);
+			} else {
+				unregister_code(KC_9);
+			}
+			break;
+		case FN_LCBRACKET:
+			if (event.pressed) {
+				if (get_mods() == MOD_BIT(KC_LSHIFT)) {  // [ if shifted
+					set_mods(MOD_BIT(KC_RALT));
+					send_keyboard_report();
+					register_code(KC_8);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else {  // { otherwise
+					set_mods(MOD_BIT(KC_RALT));
+					send_keyboard_report();
+					register_code(KC_7);
+					clear_mods();
+				}
+			} else {
+				unregister_code(KC_8);
+				unregister_code(KC_7);
+			}
+			break;
+		case FN_RCBRACKET:
+			if (event.pressed) {
+				if (get_mods() == MOD_BIT(KC_LSHIFT)) {  // ] if shifted
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_9);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else {  // } otherwise
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_0);
+					clear_mods();
+				}
+			} else {
+				unregister_code(KC_9);
+				unregister_code(KC_0);
+			}
+			break;
+		case FN_TILDE:
+			if (event.pressed) {
+				uint8_t mods = get_mods();
+				if (mods == 0) {  // ~/
+					set_mods(MOD_BIT(KC_RALT));
+					send_keyboard_report();
+					register_code(KC_TLDE);
+					unregister_code(KC_TLDE);
+					// don't want to also press space for tilde
+					register_code(KC_SPC);
+					unregister_code(KC_SPC);
+					clear_mods();
+					send_keyboard_report();
+					// (almost) always writing ~/ anyways so add the /
+					inv_key(KC_7);
+					unregister_code(KC_7);
+				} else if (mods == MOD_BIT(KC_LSHIFT)) {  // ^
+					register_code(KC_TLDE);
+				} else if (mods == MOD_BIT(KC_RALT)) {  // " 
+					clear_mods();
+					register_code(KC_TLDE);
+					set_mods(MOD_BIT(KC_RALT));
+				}
+			} else {
+				uint8_t mods = get_mods();
+				// Nothing to unregister if mods == 0, which means ~
+				if (mods != 0) {
+					unregister_code(KC_TLDE);
+				}
+			}
+			break;
+		case FN_COLON_PIPE:
+			if (event.pressed) {
+				if (get_mods() == MOD_BIT(KC_LSHIFT)) {  // | if shifted
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_LTE);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else {  // : otherwise
+					set_mods(MOD_BIT(KC_LSHIFT));
+					register_code(KC_DOT);
+					clear_mods();
+				}
+			} else {
+				unregister_code(KC_LTE);
+				unregister_code(KC_DOT);
+			}
+			break;
+		case FN_BASHSLAH_AT:
+			if (event.pressed) {
+				if (get_mods() == MOD_BIT(KC_LSHIFT)) {  // @ if shifted
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_2);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else {  // \ otherwise
+					set_mods(MOD_BIT(KC_RALT));
+					register_code(KC_PLS);
+					clear_mods();
+				}
+			} else {
+				unregister_code(KC_2);
+				unregister_code(KC_PLS);
+			}
+			break;
+		case FN_MINUS_ACCENT:
+            if (event.pressed) {
+				uint8_t mods = get_mods();
+				if (mods == 0) {  // -
+					register_code(KC_MINS);
+				} else if (mods == MOD_BIT(KC_LSHIFT)) {  // ' (accent)
+					clear_mods();
+					register_code(KC_GRV);
+					set_mods(MOD_BIT(KC_LSHIFT));
+				} else if (mods == MOD_BIT(KC_RALT)) {  // ` (grave accent)
+					set_mods(MOD_BIT(KC_LSHIFT));
+					register_code(KC_GRV);
+					set_mods(MOD_BIT(KC_RALT));
+				}
+			} else {
+				unregister_code(KC_MINS);
+				unregister_code(KC_GRV);
+			}
+			break;
+		case FN_UNDERSCR:
+			if (event.pressed) {
+				inv_key(KC_MINS);
+			} else {
+				unregister_code(KC_MINS);
+			}
+			break;
+		case SET_L0_LED_OFF:
+			if (event.pressed) {
+				set_layer0_switch_led_off();
+			}
+			break;
+		case SET_L1_WITH_LED:
+			if (event.pressed) {
+				set_layer1_with_led();
+			}
+			break;
+		case SET_L2_WITH_LED:
+			if (event.pressed) {
+				set_layer2_with_led();
+			}
+			break;
+		case SET_L3_WITH_LED:
+			if (event.pressed) {
+				set_layer3_with_led();
+			}
+			break;
+	}
+}
+
 
 
 #define KEYMAPS_SIZE    (sizeof(keymaps) / sizeof(keymaps[0]))
@@ -218,11 +559,6 @@ uint8_t keymap_key_to_keycode(uint8_t layer, key_t key)
     }
 }
 
-#if defined(KEYMAP_CUB)
-
-// function keymap_fn_to_action will be defined in keymap_cub.h
-
-#else
 /* translates Fn keycode to action */
 action_t keymap_fn_to_action(uint8_t keycode)
 {
@@ -234,5 +570,3 @@ action_t keymap_fn_to_action(uint8_t keycode)
     }
     return action;
 }
-#endif
-
